47ff128ea6b333a589801456b903bb182204f2cb
Scott <scottyboy805@hotmail.co.uk>
2024-07-20 15:21:16 +0100

    Rename assembly

--- Build_Meta-Interface_StandaloneVersion_Debug.bat
+++ Build_Meta-Interface_StandaloneVersion_Debug.bat
@@ -1,5 +1,4 @@
-msbuild "Unity - Meta-Interface/MetaInterface.csproj" -t:Rebuild -property:OutDir="../Build - Meta-Interface-Standalone" -property:DefineConstants="UNITY;DEBUG" 
+~echo off
+msbuild "Unity - Meta-Interface/Meta-Interface.csproj" -t:Rebuild -property:OutDir="../Build - Meta-Interface-Standalone" -property:DefineConstants="UNITY;DEBUG" 
 
 ".tools/pdb2mdb.exe" "Build - Meta-Interface-Standalone/Meta-Interface.dll"
-
-pause
